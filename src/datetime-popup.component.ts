84bccd0ffc4e29a7a9864cd7ce841480d18ff43a
Gillardo <Gillardo@users.noreply.github.com>
2017-07-19 12:56:45 +0100

    Updated

--- src/datetime-popup.component.ts
+++ src/datetime-popup.component.ts
@@ -3,8 +3,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
     selector: 'datetime-popup',
     template: `
-        <div class="dropdown" [ngClass]="{ 'show': showPopup === true }">
-            <ul class="dropdown-menu" role="menu" (offClick)="offClick($event)" [ngClass]="{ 'show': showPopup === true }">
+        <div class="dropdown" [ngClass]="{ 'show': showPopup === true }" (offClick)="offClick($event)">
+            <ul class="dropdown-menu" role="menu" [ngClass]="{ 'show': showPopup === true }">
                 <li class="my-2 mx-2">
                     <datepicker *ngIf="showDate" [(ngModel)]="value" 
                                 (selectionDone)="onDateChange($event)"
