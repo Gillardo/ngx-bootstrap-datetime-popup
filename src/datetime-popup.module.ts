24116480e974f6102e8ca1cf15d4778929a8e7d3
Darren <darren.gillard81@gmail.com>
2018-06-14 15:40:49 +0100

    changed import

--- src/datetime-popup.module.ts
+++ src/datetime-popup.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { DatepickerModule, DatepickerConfig } from 'ngx-bootstrap';
+import { DatepickerModule, DatepickerConfig } from 'ngx-bootstrap/datepicker';
 import { TimepickerModule, TimepickerConfig, TimepickerActions } from 'ngx-bootstrap/timepicker';
 
 import { DatetimePopupComponent, OffClickDirective } from './components';
