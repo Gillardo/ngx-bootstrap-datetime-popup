77f667f41889a1923e06b8e657d1386ef3f884c7
Shafiq Jetha <jethas@bennettjones.com>
2021-12-14 11:04:20 -0700

    Upgrade to Angular 11

--- projects/ngx-bootstrap-datetime-popup/src/lib/ngx-bootstrap-datetime-popup.component.spec.ts
+++ projects/ngx-bootstrap-datetime-popup/src/lib/ngx-bootstrap-datetime-popup.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync as  } from '@angular/core/testing';
 
 import { NgxBootstrapDatetimePopupComponent } from './ngx-bootstrap-datetime-popup.component';
 
@@ -6,7 +6,7 @@ describe('NgxBootstrapDatetimePopupComponent', () => {
   let component: NgxBootstrapDatetimePopupComponent;
   let fixture: ComponentFixture<NgxBootstrapDatetimePopupComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ NgxBootstrapDatetimePopupComponent ]
     })
