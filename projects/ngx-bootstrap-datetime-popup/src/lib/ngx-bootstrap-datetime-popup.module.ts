de2de965194a8bb65f1de7ec2d73d6ea004fe423
Shafiq Jetha <jethas@bennettjones.com>
2022-03-22 10:39:39 -0600

    feat(bootstrap): add support for ngx-bootstrap 8
    Also updates the demo application so that the input boxes work as

--- projects/ngx-bootstrap-datetime-popup/src/lib/ngx-bootstrap-datetime-popup.module.ts
+++ projects/ngx-bootstrap-datetime-popup/src/lib/ngx-bootstrap-datetime-popup.module.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BsDatepickerConfig, BsDatepickerModule, DatepickerModule } from 'ngx-bootstrap/datepicker';
+import {
+  BsDatepickerConfig,
+  BsDatepickerModule,
+} from 'ngx-bootstrap/datepicker';
 import { BsDropdownConfig, BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TimepickerModule } from 'ngx-bootstrap/timepicker';
 import { DatetimePopupComponent } from './ngx-bootstrap-datetime-popup.component';
@@ -12,12 +15,11 @@ import { OffClickDirective } from './offclick.directive';
   imports: [
     CommonModule,
     FormsModule,
-    DatepickerModule,
     TimepickerModule,
     BsDatepickerModule,
     BsDropdownModule,
   ],
   exports: [DatetimePopupComponent],
-  providers: [BsDatepickerConfig, BsDropdownConfig]
+  providers: [BsDatepickerConfig, BsDropdownConfig],
 })
 export class DatetimePopupModule {}
